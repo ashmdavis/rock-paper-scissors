5364e775d42feadc5586369dab0038db1bbcc28b
ashmdavis <68405151+ashmdavis@users.noreply.github.com>
2026-01-10 20:49:54 -0500

    selection buttons functional added score tracker and score history

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
-// create user selection buttons and container
 const container = document.querySelector("#container");
+
+//creat buttons container and append it to html container
+const buttonContainer = document.createElement("div");
+container.appendChild(buttonContainer);
+
+// create and append selection buttone to button container
 const rockButton = document.createElement("button");
-const paperButton = document.createElement("button");
-const scissorsButton = document.createElement("button");
-
-// attatch buttons to container
-container.appendChild(rockButton);
-container.appendChild(paperButton);
-container.appendChild(scissorsButton);
-
-// give each button text content
+buttonContainer.appendChild(rockButton);
 rockButton.textContent = "Rock"
+rockButton.classList.add("rock");
+
+const paperButton = document.createElement("button");
+buttonContainer.appendChild(paperButton);
 paperButton.textContent = "Paper"
+paperButton.classList.add("paper");
+
+const scissorsButton = document.createElement("button");
+buttonContainer.appendChild(scissorsButton);
 scissorsButton.textContent = "Scissors"
-
-// give each button a unique id
-rockButton.id = "rock";
-paperButton.id = "paper";
-scissorsButton.id = "scissors";
-
-// add an event lister for each button element
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        // button differentiated by id
-        alert(button.id);
-    });
-});
+scissorsButton.classList.add("scissors");
 
 // create a function for computer choice
 // return rock paper or scissors in competer choice function at random
@@ -35,28 +27,35 @@ buttons.forEach((button) => {
 function getComputerChoice() {
     const randomChoice = Math.floor(Math.random() * 3);
     if (randomChoice === 0) {
-        return "rock"
+        return "rock";
     } else if (randomChoice === 1) {
-        return "paper"
+        return "paper";
     } else {
-        return "scissors"
+        return "scissors";
     }
 }
 
-// create a function for human choice
-// return one valid user choice depending on user input
-// test human choice function
-
-function getHumanChoice() {
-    const humanInput = prompt("Rock, paper, or scissors?");
-    return humanInput.toLowerCase();
-}
+// const computerSelection = getComputerChoice();
+// console.log(computerSelection);
 
 // create variables to keep track of human score and computer score
 // set those variables to be the value of 0
 
 let computerScore = 0;
 let humanScore = 0;
+
+const scoreContainer = document.createElement("div");
+container.appendChild(scoreContainer);
+
+const humanScoreContainer = document.createElement("div");
+scoreContainer.appendChild(humanScoreContainer);
+
+
+const compScoreContainer = document.createElement("div");
+scoreContainer.appendChild(compScoreContainer);
+
+const historyContainer = document.createElement("ul");
+container.appendChild(historyContainer);
 
 // write logic to play a single round
 // create playRound function
@@ -65,48 +64,66 @@ let humanScore = 0;
 // playRound function return a string representing the round winner
 // increment score based on round winner
 
-function playRound(computerChoice, humanChoice) {
+function playRound(humanChoice, computerChoice) {
     let gameResult = "";
 
     if (humanChoice === computerChoice) {
-        gameResult = "It's a draw!"
+        gameResult = `Opponent chose ${computerChoice}. It's a Draw!`;
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        gameResult = "You win! Rock beats Scissors";
+        gameResult = `Opponent chose ${computerChoice}. You Win! Rock beats Scissors`;
         humanScore++
     } else if (humanChoice === "rock" && computerChoice === "paper") {
-        gameResult = "You lose! Paper beats Rock.";
+        gameResult = `Opponent chose ${computerChoice}. You Lose! Paper beats Rock.`;
         computerScore++
     } else if (humanChoice === "paper" && computerChoice === "rock") {
-        gameResult = "You win! Paper beats Rock";
+        gameResult = `Opponent chose ${computerChoice}. You Win! Paper beats Rock`;
         humanScore++
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        gameResult = "You lose! Scissors beat Paper.";
+        gameResult = `Opponent chose ${computerChoice}. You Lose! Scissors beat Paper.`;
         computerScore++
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        gameResult = "You lose! Rock beats Scissors";
+        gameResult = `Opponent chose ${computerChoice}. You Lose! Rock beats Scissors`;
         computerScore++
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        gameResult = "You win! Scissors beat Paper.";
+        gameResult = `Opponent chose ${computerChoice}. You Win! Scissors beat Paper.`;
         humanScore++
     }
-    return (gameResult);
+
+    humanScoreContainer.textContent = `Your Score: ${humanScore}`;
+    compScoreContainer.textContent = `Opponent Score: ${computerScore}`;
+    historyContainer.innerHTML += `<li>${gameResult}</li>`;
+    getGameWinner();
 }
+
+buttonContainer.addEventListener("click", (event) => {
+    let target = event.target;
+    switch (target.classList.value) {
+        case "rock":
+            playRound("rock", getComputerChoice());
+            break;
+        case "paper":
+            playRound("paper", getComputerChoice());
+            break;
+        case "scissors":
+            playRound("scissors", getComputerChoice());
+            break;
+    }
+});
 
 // create a function named playGame
 // move playRound inside of playGame function
 // play 5 rounds by calling play round 5 times
 
-function playGame() {
-    alert(`Opponent chose ${computerSelection}`);
-    alert(playRound(computerSelection, humanSelection));
-    alert("Game Over")
-    if (humanScore > computerScore) {
-        alert(`Your Score: ${humanScore} | Opponent Score: ${computerScore} | Congrats, Winner.`);
-    } else {
-        alert(`Your Score: ${humanScore} | Opponent Score: ${computerScore} | Try again, Loser.`);
+function getGameWinner() {
+    if (humanScore === 5 || computerScore === 5) {
+        if (humanScore > computerScore) {
+            console.log("You Win");
+        } else if (humanScore < computerScore) {
+            console.log("You lose");
+        } else {
+            console.log("it's a tie");
+        }
+        humanScore = 0;
+        computerScore = 0;
     }
 }
-
-playGame();
-
-
